5c992e296f1bf8cea43536a098f8f7c3ccecbaac
h1gaming <mail@h1gaming.com>
2017-11-25 10:56:40 +0100

    Simple Island modeled

--- Assets/Scripts/AgentChopTree.cs
+++ Assets/Scripts/AgentChopTree.cs
@@ -5,21 +5,23 @@ using UnityStandardAssets.Characters.ThirdPerson;
 
 public class AgentChopTree : MonoBehaviour {
 
-    public float quantity;
-    public float maxQuantity;
+    public float quantity = 0.0f;
+    public float maxQuantity = 5.0f;
+    public float reachDistance = 1.0f;
 
 	// Use this for initialization
 	void Start () {
         quantity = 0.0f;
         maxQuantity = 5.0f;
-	}
+        reachDistance = 1.0f;
+    }
 	
 	// Update is called once per frame
 	void Update () {
         Transform target = gameObject.GetComponent<AICharacterControl>().target;
         if (target != null)
         {
-            if (Vector3.Distance(transform.position, target.position) <= gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>().stoppingDistance+1.0f)
+            if (Vector3.Distance(transform.position, target.position) <= gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>().stoppingDistance + reachDistance)
             {
                 //Debug.Log("Collect Distance reached");
                 HandleInteraction(target);
@@ -29,15 +31,18 @@ public class AgentChopTree : MonoBehaviour {
 
     }
 
-    /*void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
-        if (gameObject.GetComponent<AICharacterControl>().target.CompareTag(other.transform.tag))
+        //Debug.Log("Collision detected");
+        if (gameObject.GetComponent<AICharacterControl>().target != null)
         {
-            Debug.Log("Correct Collision");
-            HandleInteraction(other.transform);
+            if (gameObject.GetComponent<AICharacterControl>().target.transform == other.GetComponentInParent<Transform>())
+            {
+                //Debug.Log("Correct Collision");
+                HandleInteraction(other.transform);
+            }
         }
-
-    }*/
+    }
 
     public bool HasSpace()
     {
